2ba6e22f4aea2c14c9655d1c921dc240833f3c68
Nolan Stucky <nolan@stucky.net>
2021-07-25 14:30:04 -0700

    added function to track encounters

--- movement_bot/main.py
+++ movement_bot/main.py
@@ -1,25 +1,54 @@
-import pyautogui   
+import pyautogui 
+import keyboard
+import PIL.ImageGrab
 import time
+
+DELAY_BETWEEN_COMMANDS = 0
 
 def main():
 
-    pyautogui.FAILSAFE = True
+    pyAutoGuiFailSafe()
+    countDownTimer()
 
+    # startEncounters()
+
+
+
+    # holdKey('w')
+    # holdKey('s')
+
+    print(pyautogui.position())
+
+    
+
+def countDownTimer():
     # Countdown timer
     print("Starting", end="")
     for i in range(0, 5):
-        print(".", end="")
+        print(".", end="", flush=True)
         time.sleep(1)
     print("Go")
 
-    pyautogui.keyDown('w')
-    time.sleep(0.5)
-    pyautogui.keyUp('w')
+def holdKey(key, seconds=0.5):
+    pyautogui.keyDown(key)
+    time.sleep(seconds)
+    pyautogui.keyUp(key)
+    time.sleep(DELAY_BETWEEN_COMMANDS)
 
-    pyautogui.keyDown('s')
-    time.sleep(0.5)
-    pyautogui.keyUp('s')
+def pyAutoGuiFailSafe():
+    pyautogui.FAILSAFE = True
 
+def startEncounters():
+    NUMBER_OF_ENCOUNTERS = 0
+    while NUMBER_OF_ENCOUNTERS < 500:
+        if keyboard.is_pressed('k'):
+            break
+
+        
+        print(NUMBER_OF_ENCOUNTERS)
+        time.sleep(1)
+
+        NUMBER_OF_ENCOUNTERS += 1
 
 if __name__ == "__main__":
     main()
